3813500d153a187223187effff0f9b3ec955ab8e
Andrew <20448589@life.hkbu.edu.hk>
2021-07-27 17:36:45 +0800

    a

---
+++ Dish.java
@@ -0,0 +1,13 @@
+public class Dish{
+    // attrubute
+    int id;
+    String name;
+    double price;
+
+    //build consturter
+    public Dish(int id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ Orderingapp.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Orderingapp {
+
+    static List<Dish> dishlist = new ArrayList<>();
+
+
+    public static void main(String[] args){
+        // need have food first!
+        // Dish dish = new Dish(1, "Sandwich", 29.0);
+        // Dish dish2 = new Dish(2, "French Fries", 10.0);
+        initDish(); 
+        // We will have 5 dishs in our res
+        
+        // System.out.println(dishlist);
+        // Create a Scanner
+        Scanner s = new Scanner(System.in);
+        // System.out.println("Please input The Id");
+        // // Get the content
+        // int num = s.nextInt();
+        // System.out.println(num);
+        //Show the dish menu
+        // showMenu();
+        // int num = s.nextInt();
+
+        while (true){
+            showMenu();
+            int num = s.nextInt();
+            switch (num){
+                case 1:
+                    showDishMenu();
+                    break;
+                case 2:
+                    showPersonDish();
+                    break;
+                case 3:
+                    buy();
+                    return;
+            }
+
+        }
+
+    }
+
+    public static void showDishMenu(){
+        
+    }
+
+    public static void showPersonDish(){
+
+    }
+
+    public static void buy(){
+
+    }
+
+    
+    public static void showMenu(){
+        System.out.println("---Menu---");
+        System.out.println("Menu\t\t\t 1");
+        System.out.println("Ordered\t\t\t 2");
+        System.out.println("Pay\t\t\t 3");
+        System.out.println("Please Input Number to Choose");
+    }
+
+
+    public static void initDish(){
+        Dish dish = new Dish(1, "Sandwich", 29.0);
+        dishlist.add(dish);
+        Dish dish2 = new Dish(2, "French Fries", 10.0);
+        dishlist.add(dish2);
+
+        dishlist.add(new Dish(3, "Caesar Salad", 12.0));
+        dishlist.add(new Dish(4, "Salisbury Steak", 100.0));
+        dishlist.add(new Dish(5, "Carpaccio", 30.0));
+    }   
+
+}
